5c30dd975680fbaf1f6c847ad73706079c2cdb52
Venus Rodin <VenusViolet1234@gmail.com>
2023-04-04 21:27:01 -1000

    index.js coming along nicely

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is the name of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a short description of your project:',
+  },
+  // Add more questions here...
+])
+.then((answers) => {
+  // Store the user's answers in an object
+  console.log(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  // Create the README template using the user's answers
+  const readmeTemplate = `
+    # ${answers.projectName}
 
-// TODO: Create a function to initialize app
-function init() {}
+    ## Description
 
-// Function call to initialize app
-init();
+    ${answers.description}
+
+    ### Installation
+
+    ${answers.installation}
+
+    ### Usage
+
+    ${answers.usage}
+
+    ### License
+
+    ${answers.license}
+  `;
+
+  // Write the README file
+  fs.writeFile('README.md', readmeTemplate, (err) => {
+    if (err) throw err;
+    console.log('README.md file created successfully!');
+  });
+});
